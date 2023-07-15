d403909c792181f9352ab321787e612b1f316a2b
Igor Nascimento <igornascimentodesouza@gmail.com>
2023-07-15 16:46:00 -0400

    endpoints de retorno de acordo com os filtros

--- src/main/java/br/com/banco/endpoint/TransferenciaEndpoint.java
+++ src/main/java/br/com/banco/endpoint/TransferenciaEndpoint.java
@@ -1,11 +1,16 @@
 package br.com.banco.endpoint;
 
 import br.com.banco.repository.TransferenciaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @RestController
 @RequestMapping("v1")
@@ -20,6 +25,24 @@ public class TransferenciaEndpoint {
     @GetMapping(path = "transferencias")
     public ResponseEntity<?> listAll(){
         return new ResponseEntity<>(transferenciaDAO.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "transferencias/{dataInicio}/{dataFim}")
+    public ResponseEntity<?> findTransferenciaByData(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicio,
+                                                     @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFim){
+        return new ResponseEntity<>(transferenciaDAO.findByDataTransferenciaBetween(dataInicio, dataFim), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "transferencias/{nomeOperador}")
+    public ResponseEntity<?> findTransferenciaByNomeOperador(@PathVariable String nomeOperador){
+        return new ResponseEntity<>(transferenciaDAO.findByNomeOperadorTransacaoIgnoreCaseContaining(nomeOperador), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "transferencias/{nomeOperador}/{dataInicio}/{dataFim}")
+    public ResponseEntity<?> findByNomeOperadorAndDataTransferencia(@PathVariable String nomeOperador,
+                                                                    @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicio,
+                                                                    @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFim){
+        return new ResponseEntity<>(transferenciaDAO.findByNomeOperadorTransacaoIgnoreCaseContainingAndDataTransferenciaBetween(nomeOperador, dataInicio, dataFim), HttpStatus.OK);
     }
 
 }
--- src/main/java/br/com/banco/repository/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repository/TransferenciaRepository.java
@@ -3,5 +3,15 @@ package br.com.banco.repository;
 import br.com.banco.model.Transferencia;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface TransferenciaRepository extends PagingAndSortingRepository<Transferencia, Long> {
+
+    List<Transferencia> findByDataTransferenciaBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    List<Transferencia> findByNomeOperadorTransacaoIgnoreCaseContaining(String nome);
+
+    List<Transferencia> findByNomeOperadorTransacaoIgnoreCaseContainingAndDataTransferenciaBetween(String nome, LocalDateTime inicio, LocalDateTime fim);
+
 }
